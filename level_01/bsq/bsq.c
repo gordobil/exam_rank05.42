95ee7772be4c9092781a0843bff81df988f9f86b
gordobil <noahgordobil@gmail.com>
2026-05-05 13:51:23 +0200

    practice

--- level_01/bsq/bsq.c
+++ level_01/bsq/bsq.c
@@ -1,5 +1,6 @@
 
-typedef struct	s_map{
+typedef struct	s_map
+{
 	int		_rows;
 	int		_cols;
 	char	**_map;
@@ -8,12 +9,14 @@ typedef struct	s_map{
 	char	_fC;
 }				t_map;
 
+#define _POSIX_C_SOURCE 200809L
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 void	freeMap(int r, t_map *m){
-	if (!m || !m->_map || r < 0)
+	if (!m || !m->_map || r < 0 || !m->_map[r])
 		return ;
 	
 	for (; r >= 0; --r)
@@ -36,8 +39,7 @@ void	solveMap(t_map *m){
 
 	for (int x = 0; x < m->_rows; ++x){
 		for (int y = 0; y < m->_cols; ++y){
-			int	s = 1;
-			for (;x + s <= m->_rows && y + s <= m->_cols; ++s){
+			for (int s = 1;(x + s) < m->_rows && (y + s) < m->_cols; ++s){
 				if (obstacle(m, x, y, s))
 					break ;
 				if (s > bS){
@@ -50,9 +52,9 @@ void	solveMap(t_map *m){
 	}
 
 	if (bS > 0){
-		for (int x = 0; x < bS; ++x){
-			for (int y = 0; y < bS; ++y)
-				m->_map[bX + x][bY + y] = m->_fC;
+		for (int x = bX; x < (bX + bS); ++x){
+			for (int y = bY; y < (bY + bS); ++y)
+				m->_map[x][y] = m->_fC;
 		}
 	}
 
@@ -61,13 +63,12 @@ void	solveMap(t_map *m){
 		fputs(m->_map[i], stdout);
 		fputc('\n', stdout);
 	}
-	fputc('\n', stdout);
 }
 
 int		getMap(FILE *fd, t_map *m){
 	m->_map = (char **)calloc(m->_rows + 1, sizeof(char *));
 	if (!m->_map)
-		return (fputs("\nError reserving map memory.\n", stderr), -1);
+		return (fputs("\nError: error reserving map memory.\n", stderr), -1);
 	m->_map[m->_rows] = NULL;
 
 	char	*line = NULL;
@@ -77,37 +78,37 @@ int		getMap(FILE *fd, t_map *m){
 	for (int r = 0; r < m->_rows; ++r){
 		len = getline(&line, &n, fd);
 		if (len <= 1 || !line || line[len - 1] != '\n')
-			return (fputs("\nError getting line\n", stderr), freeMap(r - 1, m), free(line), -1);
+			return (fputs("\nError: error getting line\n", stderr), freeMap(r - 1, m), free(line), -1);
 		
-		line[len - 1] = '\0';
 		len--;
+		line[len] = '\0';
 
 		if (r == 0)
 			m->_cols = len;
 		else if (r > 0 && len != m->_cols)
-			return (fputs("\nInvalid line length.\n", stderr), freeMap(r - 1, m), free(line), -1);
+			return (fputs("\nError: invalid line lenght.\n", stderr), freeMap(r - 1, m), free(line), -1);
 		
 		for (int i = 0; i < m->_cols; ++i){
 			if (line[i] != m->_eC && line[i] != m->_oC)
-				return (fputs("\nInvalid character.\n", stderr), freeMap(r, m), free(line), -1);
+				return (fputs("\nError: invalid character in map.\n", stderr), freeMap(r, m), free(line), -1);
 		}
 
 		m->_map[r] = (char *)malloc(m->_cols + 1);
 		if (!m->_map[r])
-			return (fputs("\nError reserving line memory.\n", stderr), freeMap(r, m), free(line), -1);
+			return (fputs("\nError: error reserving line memory.\n", stderr), freeMap(r, m), free(line), -1);
 		for (int i = 0; i <= m->_cols; ++i)
 			m->_map[r][i] = line[i];
 	}
 	return (free(line), 0);
 }
 
-int		checkMap(FILE *fd, t_map *m){
+int		checkHeader(FILE *fd, t_map *m){
 	int		rows = 0;
 	char	e = 0, o = 0, f = 0;
 	
 	int		total = fscanf(fd, " %d %c %c %c \n", &rows, &e, &o, &f);
 	if (total != 4 || rows <= 0 || e == o || e == f || o == f)
-		return (fputs("\nInvalid header.\n", stderr), -1);
+		return (fputs("\nError: invalid header.\n", stderr), -1);
 	
 	m->_rows = rows;
 	m->_eC = e;
@@ -116,16 +117,13 @@ int		checkMap(FILE *fd, t_map *m){
 	m->_cols = 0;
 	m->_map = NULL;
 
-	if (getMap(fd, m) != 0)
-		return (-1);
-	
 	return (0);
 }
 
 int		bsq(FILE *fd){
 	t_map	m;
 
-	if (checkMap(fd, &m) != 0)
+	if (checkHeader(fd, &m) != 0 || getMap(fd, &m) != 0)
 		return (-1);
 	
 	solveMap(&m);
@@ -140,10 +138,11 @@ int		main(int argc, char **argv){
 
 	for (int i = 1; i < argc; ++i){
 		FILE	*fd = fopen(argv[i], "r");
-		if (!fd)
-			return (fputs("\nError processing map.\n", stderr), -1);
-		bsq(fd);
-		fclose(fd);
+		if (fd){
+			bsq(fd);
+			fclose(fd);
+			fputc('\n', stdout);
+		}
 	}
 
 	return (0);
